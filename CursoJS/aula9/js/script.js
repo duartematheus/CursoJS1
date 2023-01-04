e85e8033da7c29388e0de1ce6c10f8686d4f76da
duartematheus <32654616+duartematheus@users.noreply.github.com>
2023-01-04 16:14:50 -0300

    continuaçao

--- CursoJS/aula9/js/script.js
+++ CursoJS/aula9/js/script.js
@@ -1,3 +1,12 @@
+/*
 alert('Nossa mensagem');
 confirm('Deseja realmente apagar ?');
 prompt('Digite seu nome:');
+*/
+let num1 = prompt('Digite um número');
+
+let num2 = prompt('Digite um numero');
+num1 = Number(num1);
+num2 = Number(num2);
+const resultado = num1 + num2;
+alert(`Resultado ${resultado}`);
