c77b0716991cbe1a4bc3ec87bf8ad607b9df3cd3
duartematheus <32654616+duartematheus@users.noreply.github.com>
2023-01-20 15:50:14 -0300

    objetos em js

---
+++ CursoJS/aula17/objetos.js
@@ -0,0 +1,47 @@
+const fulano = [1,2,3];// para arrais utiliza [] e para objeto utiliza {}
+fulano.push(4);
+fulano[0] = 'Luiz';
+console.log(fulano);
+
+//////////////////////////////////////////////
+
+const pessoa1 = {
+    nome:'Matheus',
+    sobrenome:'Rocha',
+    idade: 33
+};
+console.log(pessoa1.nome);
+console.log(pessoa1.idade);
+
+////////////Função ///////////////////////////
+
+function criaPessoa(nomes,sobrenomes,idades){
+    return {
+        nome1: nomes,
+        sobrenome1:sobrenomes,
+        idade1: idades
+    };
+}
+
+const pessoa2 = criaPessoa('Fernanda','Jessica',25);
+const pessoa3 = criaPessoa('José','Silva',45);
+console.log(pessoa2);
+console.log(pessoa3);
+
+//////// outro exemplo/////////////////////
+const pessoa4 = {
+    nome2: 'Maria',
+    sobrenome2:'Ang',
+    idade2:58,
+    // criar uma função fala 
+    fala() {
+        console.log(`${this.nome2} ${this.sobrenome2} esta falando oi...`);
+        console.log(`A minha idade é essa ${this.idade2}`)
+    },
+    incrementaIdade() {
+        this.idade2++;
+    }
+};
+ pessoa4.fala();
+ pessoa4.incrementaIdade();
+ pessoa4.fala();
