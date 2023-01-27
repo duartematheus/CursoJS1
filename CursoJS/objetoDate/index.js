1cfdf8969944c94632f7b8f9ddfb27939b70d04f
duartematheus <32654616+duartematheus@users.noreply.github.com>
2023-01-27 14:47:34 -0300

    proxima aula switch

--- CursoJS/objetoDate/index.js
+++ CursoJS/objetoDate/index.js
@@ -5,11 +5,30 @@ const data2 = new Date('2023-01-27 14:07:35.340');
 console.log(data2.toString());
 
 console.log('Dia', data.getDate());
-console.log('Mês', data.getMonth());
+console.log('Mês', data.getMonth()); //Mes começa do 0
 console.log('Ano', data.getFullYear());
 console.log('Hora', data.getHours());
 console.log('Min', data.getMinutes());
 console.log('Seg', data.getSeconds());
 console.log('ms', data.getMilliseconds());
-console.log('Dia na semana',data.getDay(),'ª');
+console.log('Dia na semana',data.getDay(),'ª'); //0- Domingo, 6-Sabado 
 
+//Outro exemplo
+function zeroAEsquerda (num){
+    return num >= 10 ? num : `0${num}`;
+}
+function formataData(valor){
+   const dia = zeroAEsquerda(valor.getDate());
+   const mes = zeroAEsquerda(valor.getMonth() +1);
+   const ano = zeroAEsquerda(valor.getFullYear());
+   const hora = zeroAEsquerda(valor.getHours());
+   const min = zeroAEsquerda(valor.getMinutes());
+   const seg = zeroAEsquerda(valor.getSeconds());
+   
+   return `${dia}/${mes}/${ano}  ${hora}:${min}:${seg}`;
+
+}
+
+const data4 = new Date();
+const dataBrasil = formataData(data4);
+console.log(dataBrasil);
