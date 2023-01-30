756969f3fe18b4c5b2a51146b387392252b0adfb
duartematheus <32654616+duartematheus@users.noreply.github.com>
2023-01-30 09:27:08 -0300

    continuação

---
+++ CursoJS/SwitchCase/index.js
@@ -0,0 +1,56 @@
+const data = new Date();
+let diaSemana = data.getDay();
+let diaSemanaTexto;
+//Forma simples
+switch (diaSemana){
+    case 0:
+        diaSemanaTexto = 'Domingo';
+        break;
+    case 1:
+        diaSemanaTexto = 'Segunda';
+        break;
+    case 2:
+        diaSemanaTexto = 'Terça';
+        break;
+    case 3:
+        diaSemanaTexto = 'Quarta';
+        break;
+    case 4:
+        diaSemanaTexto = 'Quinta';
+        break;
+    case 5:
+        diaSemanaTexto = 'Sexta';
+        break;
+    case 6:
+        diaSemanaTexto = 'Sabado';
+        break;
+}
+
+console.log(diaSemanaTexto,getSemanaDiaTexto(diaSemana));
+
+//Forma usando função
+function getSemanaDiaTexto(diaSemana){
+    switch (diaSemana){
+        case 0:
+            diaSemanaTexto = 'Domingo';
+            return diaSemanaTexto;
+        case 1:
+            diaSemanaTexto = 'Segunda';
+            return diaSemanaTexto;
+        case 2:
+            diaSemanaTexto = 'Terça';
+            return diaSemanaTexto;
+        case 3:
+            diaSemanaTexto = 'Quarta';
+            return diaSemanaTexto;
+        case 4:
+            diaSemanaTexto = 'Quinta';
+            return diaSemanaTexto;
+        case 5:
+            diaSemanaTexto = 'Sexta';
+            return diaSemanaTexto;
+        case 6:
+            diaSemanaTexto = 'Sabado';
+            return diaSemanaTexto;
+    }    
+}
