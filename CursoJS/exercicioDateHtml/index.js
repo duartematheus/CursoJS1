6d5276d83f75a4470b0a6050c86da3f082d64b45
duartematheus <32654616+duartematheus@users.noreply.github.com>
2023-01-31 13:45:13 -0300

    continuação

--- CursoJS/exercicioDateHtml/index.js
+++ CursoJS/exercicioDateHtml/index.js
@@ -1,4 +1,38 @@
+function dataSite(valor){
+    const diaSemana='';
+    const dia = valor.getDay();
+    //const data = valor.getDate();
+    //const hora = valor.getHours(); 
+    if ( dia == 0){
+        diaSemana = 'Domingo';
+    }
+    else if ( dia == 1){
+        diaSemana == 'Segunda';
+    }
+    else if ( dia == 2){
+        diaSemana = 'Terça-Feira';
+    }
+    else if ( dia == 3){
+        diaSemana = 'Quarta-Feira';
+    }
+    else if ( dia == 5){
+        diaSemana = 'Quinta-Feira';
+    }
+    else if ( dia == 6){
+        diaSemana = 'Sexta-Feira';
+    }
+    else if (dia == 7){
+        diaSemana ='Sábado';
+    }
+    else{
+        diaSemana = '';
+    }
+   
+    return diaSemana;
+}
+
 const hoje = new Date();
-const mostrarData = document.querySelector('#data')
-mostrarData.innerHTML = hoje;
-console.log(hoje);
+dataSite(hoje);
+//const mostrarData = document.querySelector('#data');
+//const diaLiteral = dataSite(hoje);
+//mostrarData.innerHTML = diaLiteral;
