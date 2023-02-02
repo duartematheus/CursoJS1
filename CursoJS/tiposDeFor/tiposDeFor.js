c98d8586e006794853683a1a6403368ecfa0d151
duartematheus <32654616+duartematheus@users.noreply.github.com>
2023-02-02 15:34:54 -0300

    continuação

---
+++ CursoJS/tiposDeFor/tiposDeFor.js
@@ -0,0 +1,14 @@
+const frutas = ['maçã', 'uva', 'pera', 'limão'];
+console.log('-----for in -----');
+
+for (let i in frutas){
+    console.log(frutas[i]);//mostra o valor do indice no array
+    console.log(i);//mostra o indeice do array
+}
+
+console.log('-----for of -----');
+
+for (let i of frutas){
+    console.log(frutas[i]);// -> undefined
+    console.log(i);//mostra o valor do indice no array
+}
