b0ddb804d1616a7f748e9e7607251b1d12f83f50
duartematheus <32654616+duartematheus@users.noreply.github.com>
2023-02-07 14:55:42 -0300

    setInterval and setTimeout

---
+++ CursoJS/setIntervalsetTimeout/index.js
@@ -0,0 +1,24 @@
+function mostraHora(){
+    let data = new Date();
+
+    return data.toLocaleTimeString('pt-BR',{hour12: false});
+      
+}
+
+//function mostraHora2(){ essa função utiliza a mostra hora e retorna o valor desta função
+//    console.log(mostraHora());
+//}
+
+// setInterval(mostraHora2,1000);  dessa forma a hora é mostrada de 1 em 1 segundo
+//_____________________________
+//Para limitar o tempo que será exibido deve-se utilizar o setTimeout
+const timer = setInterval(function(){ console.log(mostraHora());}, 1000);
+
+setTimeout(function() { //setTimeout neste caso vai contar 5seg ou 5000ms
+    clearInterval(timer);
+    console.log('Aguarde a mensagem em 3 segundos...')
+}, 5000);
+
+setTimeout(function(){
+    console.log('Obrigado');
+}, 8000);
