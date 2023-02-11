2d7a03edeae4b36f7634a313b417e6b445a59c8b
duartematheus <32654616+duartematheus@users.noreply.github.com>
2023-02-11 01:52:29 -0300

    c

--- CursoJS/exercicioTimer/index.js
+++ CursoJS/exercicioTimer/index.js
@@ -1,33 +1,40 @@
-let hora = 0;
-let min = 0;
-let seg = 0;
 
-while( hora < 24){
-       
-    if (min <= 59){
-        
-        if(seg < 59){
-           
-            console.log( `${zero(hora)}:${zero(min)}:${zero(seg)}`);
-            seg +=1;             
-            
-        }else {
-            min += 1;
-                
-            seg = 0;   
-            
-        }
-       
-    }else {
-        min = 0;
-        hora +=1;
+
+function mostraTempo(){
+    let hora = 0;
+    let min = 0;
+    let seg = 0;
+    const tempo = document.querySelector('.cronometro');
+   
+    while( hora < 24){
                
-    }
-    
-          
+        if (min <= 59){
+            
+            if(seg < 59){
+                          
+                seg +=1;             
+                
+            }else {
+                min += 1;
+                    
+                seg = 0;   
+                
+            }
+           
+        }else {
+            min = 0;
+            hora +=1;
+                   
+        }  
+        setInterval(function(){
+            tempo.innerHTML = (`${zero(hora)}:${zero(min)}:${zero(seg)}`);
+        }, 1000)  
+                
+        
+    }  
+     
     
 }
-
 
 function zero(num){
     if (num < 10){
@@ -36,3 +43,6 @@ function zero(num){
         return num;
     }
 }
+
+
+
