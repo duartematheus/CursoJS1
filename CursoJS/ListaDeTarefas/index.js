6614296bf8c4b0795d410c63db0154c2fe9ef957
duartematheus <32654616+duartematheus@users.noreply.github.com>
2023-02-18 01:13:12 -0300

    continuação

---
+++ CursoJS/ListaDeTarefas/index.js
@@ -0,0 +1,7 @@
+const inputTarefa = document.querySelector('.input-tarefa');
+const btnTarefa = document.querySelector('.btn-Adicionar');
+const tarefas = document.querySelector('.tarefas');
+
+btnTarefa.addEventListener('click',function(){
+    
+})
