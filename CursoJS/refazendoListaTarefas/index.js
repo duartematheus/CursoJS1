656ebc3627f5e2dff14f0e5de850633d5afb52ef
duartematheus <32654616+duartematheus@users.noreply.github.com>
2023-02-23 01:31:24 -0300

    continua

--- CursoJS/refazendoListaTarefas/index.js
+++ CursoJS/refazendoListaTarefas/index.js
@@ -3,3 +3,30 @@ const btnAdicionar = document.querySelector(".btnAdicionar");
 const tarefas = document.querySelector(".tarefas");
 
 
+function criaLista(){
+    const Li = document.createElement('li');
+    return Li;
+}
+
+function criaTarefa(texto){
+    const lista = criaLista();
+    lista.innerText = texto;    
+    tarefas.appendChild(lista);
+    btnApagar(lista);
+    inputTarefas.value = '';
+    
+}
+
+btnAdicionar.addEventListener('click',function(){
+    if(!inputTarefas.value) return;
+    criaTarefa(inputTarefas.value);
+})
+
+function btnApagar(Li){
+    Li.innerText += '  ';
+    const btnApagar = document.createElement('button');
+    btnApagar.innerText = 'Apagar';
+    btnApagar.setAttribute = ('class','apagar');
+    btnApagar.setAttribute = ('title','Apagar texto');
+    Li.appendChild(btnApagar);
+}
