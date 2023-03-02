7e4ccb7306226b75ce3ef4d2c1f6f03b3d48a2b5
duartematheus <32654616+duartematheus@users.noreply.github.com>
2023-03-02 14:10:51 -0300

    continuacao

--- CursoJS/calculadora/script.js
+++ CursoJS/calculadora/script.js
@@ -10,9 +10,37 @@ function criaCalculadora() {
         },
 
         btnEnviaDisplay(valor) {
-            this.display.value += valor;
-
+            
+            valor1= parseInt(valor);
+            valor2= 
+            if(valor1 === NaN){
+                if(valor === '+'){
+                    valor1 += 
+                }
+                if(valor === '-'){
+                    
+                }
+                if(valor === '/'){
+                    
+                }
+                if(valor === '*'){
+                    
+                }
+            }
+            this.display.value += valor1;   
+                            
+            
         },
+
+        btnClearDisplay(){
+            this.display.value = '';
+        },
+
+        btnApagaUm(){
+            this.display.value = this.display.value.slice(0 , -1); 
+        },
+
+        
 
         clickBotoes(){
             document.addEventListener('click', function(e) {
@@ -20,6 +48,19 @@ function criaCalculadora() {
 
                 if(el.classList.contains('btn-num')) {
                     this.btnEnviaDisplay(el.innerText);//pega o valor dentro do btn no html
+                }
+
+                if(el.classList.contains('btn-Clear')){
+                    this.btnClearDisplay();
+                }
+
+                if(el.classList.contains('btn-apagarUm')){
+                    this.btnApagaUm();
+                }
+
+                if(el.classList.contains('btn-igual')){
+                    const resultado = this.display.value;
+                    console.log(resultado);
                 }
             }.bind(this));
         },
