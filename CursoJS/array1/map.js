cb757e72c49c8341eeafde1cbb32ec992f78d94d
duartematheus <32654616+duartematheus@users.noreply.github.com>
2023-03-13 00:02:05 -0300

    continuaçao

--- CursoJS/array1/map.js
+++ CursoJS/array1/map.js
@@ -6,3 +6,18 @@ const dobrandoValores = array1.map(function(valor, indice, array){
     
 });
 console.log(dobrandoValores);
+
+//Retorne uma string com nome de uma pessoa;
+//Remova apenas a chave nome do objeto;
+//Adicione uma chave id em cada objeto;
+const pessoas = [
+    {nome: ' Matheus', idade: 33},
+    {nome: 'Luana', idade: 34},
+    {nome: 'Hugo', idade: 63},
+    {nome: 'Luiz', idade: 43}
+];
+
+const retorneNome = pessoas.map(function(valor){
+    return valor.nome;
+});
+console.log(retorneNome);
