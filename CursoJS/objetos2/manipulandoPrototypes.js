5defd12c18dd2a878d3293ff6a2a42341084e0c9
duartematheus <32654616+duartematheus@users.noreply.github.com>
2023-03-23 00:19:19 -0300

    continua

--- CursoJS/objetos2/manipulandoPrototypes.js
+++ CursoJS/objetos2/manipulandoPrototypes.js
@@ -30,7 +30,41 @@ Produto.prototype.aumento = function(percentual){
 
 const p1= new Produto('Camiseta', 50);
 const p2= (p1);
+const p3= {
+    nome: 'Prato',
+    preco: 40
+};
+Object.setPrototypeOf(p3, Produto.prototype);
+p3.aumento(30);
+
+const p4 = Object.create(Produto.prototype, {
+    preco:{
+        writable: true,
+        configurable: true,
+        enumerable: true,
+        value: 24
+    },
+    tamanho: {
+        writable: true,
+        configurable: true,
+        enumerable: true,
+        value: 42        
+    },
+    nome:{
+        writeble: true,
+        configurable: true,
+        enumerable: true,
+        value:'Boné'
+    }
+});
+
+//// atribuindo valores aos prototipes
 p1.desconto(100);
 p2.aumento(20);
+p3.aumento(30);
+p4.aumento(50);
+
 console.log(p1);
 console.log(p2);
+console.log(p3);
+console.log(p4);
