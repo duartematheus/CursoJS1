ee5b18c261852ba198a7c497fceec54e94dc32bc
duartematheus <32654616+duartematheus@users.noreply.github.com>
2023-03-29 11:04:12 -0300

    continuacao

---
+++ CursoJS/objetos2/exercicioCPF.js
@@ -0,0 +1,54 @@
+function ValidaCPF(cpfEnviado){
+    Object.defineProperty(this,'cpfLimpo', {
+        enumerable: true,
+        get: function(){
+            return cpfEnviado.replace(/\D+/g, ''); //tudo que for diferente de numero será substituido po nada.
+            
+        }
+    });
+}
+
+ValidaCPF.prototype.valida = function() {
+    if(typeof this.cpfLimpo === 'undefined') return false;
+    if(this.cpfLimpo.length !== 11) return false;
+    if(this.isSequencia()) return false;
+
+
+    const cpfParcial = this.cpfLimpo.slice(0 ,-2); //pega os 9 primeiros digitos do cpf
+    const digito1 = this.criaDigito(cpfParcial);
+    const digito2 = this.criaDigito(cpfParcial + digito1);
+    
+    const novoCpf = cpfParcial + digito1 + digito2;
+    return novoCpf === this.cpfLimpo;
+   
+};
+
+ValidaCPF.prototype.criaDigito  = function(cpfParcial) {
+    const cpfArray = Array.from(cpfParcial);
+    let regressivo = cpfArray.length + 1;
+    const total = cpfArray.reduce((ac,val) => {
+        //console.log(regressivo, typeof val, regressivo * val);
+        ac += (regressivo * Number(val));
+        regressivo--;
+        return ac;
+    },0);
+
+    const digito = 11 - (total % 11);
+    return digito > 9 ? '0' : String(digito);  //se o dito maior que 9 retorna 0 caso contrario retorna o digito
+
+};
+
+ValidaCPF.prototype.isSequencia = function() {
+    const sequencia = this.cpfLimpo[0].repeat(this.cpfLimpo.length);
+    return sequencia === this.cpfLimpo;
+
+};
+
+
+const cpf = new ValidaCPF('070.987.720-03');
+
+if(cpf.valida()){
+    console.log('Cpf válido')
+}else{
+    console.log('Cpf inválido');
+};
