31f7e4d5d5ca941de817a6148de7acc67b9e5dc9
duartematheus <32654616+duartematheus@users.noreply.github.com>
2023-04-03 14:16:05 -0300

    continuação

---
+++ CursoJS/objetos2/polimorfismo.js
@@ -0,0 +1,59 @@
+//Superclass
+function Conta(agencia, conta, saldo){
+    this.agencia = agencia;
+    this.conta = conta;
+    this.saldo = saldo;
+}
+
+Conta.prototype.sacar = function(valor){
+    if(this.saldo < valor) {
+        console.log(`Saldo insuficiente: ${this.saldo}`);
+        return;
+    }
+
+    this.saldo -= valor;
+    this.verSaldo();
+};
+Conta.prototype.depositar = function(valor){
+    this.saldo += valor;
+    this.verSaldo();
+};
+
+Conta.prototype.verSaldo = function(){
+    console.log(` Ag/c: ${this.agencia}/${this.conta}`);
+    console.log(` Saldo:R$${this.saldo.toFixed(2)}`)
+};
+
+function ContaCorrente(agencia, conta, saldo, limite){
+    Conta.call(this, agencia, conta, saldo);
+    this.limite = limite;
+}
+ContaCorrente.prototype = Object.create(Conta.prototype);
+ContaCorrente.prototype.constructor = ContaCorrente;
+
+ContaCorrente.prototype.sacar = function(valor){
+    if((this.saldo + this.limite) < valor) {
+        console.log(`Saldo insuficiente: ${this.saldo}`);
+        return;
+    }
+
+    this.saldo -= valor;
+    this.verSaldo();
+};
+
+function ContaPoupanca(agencia, conta, saldo){
+    Conta.call(this, agencia, conta, saldo);
+  
+}
+ContaPoupanca.prototype = Object.create(Conta.prototype);
+ContaPoupanca.prototype.constructor = ContaPoupanca;
+
+const contaC = new ContaCorrente(11 , 1213, 0, 100.00);
+contaC.depositar(300);
+contaC.sacar(500);
+console.log('-----------------');
+
+const contaP = new ContaPoupanca(11, 1213, 0);
+contaP.depositar(200);
+contaP.sacar(190);
+contaP.sacar(20);
