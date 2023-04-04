8f58fa9cabe04e048edd0ce91cd8eff7873cf4ea
duartematheus <32654616+duartematheus@users.noreply.github.com>
2023-04-04 14:34:34 -0300

    continuaçao

---
+++ CursoJS/objetos2/objetoMap.js
@@ -0,0 +1,24 @@
+const pessoas = [
+    {id:3, nome: 'Luiza'},
+    {id:2, nome: 'Matheus'},
+    {id:1, nome: 'Angélica'}
+];
+
+const novasPessoas = new Map();
+for (const pessoa of pessoas){
+    const { id } = pessoa;
+    novasPessoas.set(id, {...pessoa});
+}
+
+for (const [identificador, {id, nome}] of novasPessoas){
+    console.log(identificador, id, nome);
+}
+
+for (const pessoas of novasPessoas.keys()){
+    console.log(pessoas);
+}
+console.log(novasPessoas);
+console.log(novasPessoas.get(2));
+novasPessoas.delete(2);
+console.log(novasPessoas);
+
