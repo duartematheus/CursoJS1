6881e2c42bccbafcdd25b0ca8d69bcc5265c65c9
duartematheus <32654616+duartematheus@users.noreply.github.com>
2023-06-15 14:30:18 -0300

    continuar

--- CursoJS/objetos2/formulario/formulario.js
+++ CursoJS/objetos2/formulario/formulario.js
@@ -1,27 +1,12 @@
-class Formulario{
-    constructor(){
+class ValidaFormulario {
+    constructor() {
         this.formulario = document.querySelector('.formulario');
-        this.EventSource();
+        this.eventos();
     }
+
     eventos(){
-        this.formulario.addEventlistener('submit' , e=>{
-            this.handleSubmit(e);
-        });
-    }
-    handleSubmit(e){
-        e.preventDefaut();
-        const camposValidos = this.camposSaoValidos();
-        const senhasValidas = this.senhasSaoValidas();
-        if(camposValidos && senhasValidas){
-            alert('Formulario enviado');
-            this.formulario.submit();
-        }
 
-    }
-    senhasSaoValidas(){
-        let valid = true;
-
-        const senha = this.formulario.querySelector('.senha');
-        const repetirSenha = this.formulario.querySelector('.repetir-senha')
     }
 }
+
+const valida = ValidaFormulario();
