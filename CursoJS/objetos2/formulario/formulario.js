753dc8b313b85e8b6b0b2549a83f1cedfeb8681d
duartematheus <32654616+duartematheus@users.noreply.github.com>
2023-06-16 01:13:04 -0300

    continuacao

--- CursoJS/objetos2/formulario/formulario.js
+++ CursoJS/objetos2/formulario/formulario.js
@@ -5,8 +5,39 @@ class ValidaFormulario {
     }
 
     eventos(){
+        this.formulario.addEventListener('submit', e => {
+            this.handleSubmit(e);
+        });
 
     }
+
+    handleSubmit(e){
+        e.preventDefault();
+        //console.log('Formulário não enviado...');
+        const camposValidos = this.camposSaoValidos();
+        
+
+    }
+
+    camposSaoValidos(){
+        let valid = true;
+
+        for(let campo of this.formulario.querySelectorAll('.validar')){
+            //console.log(campo);
+            if(!campo.value){
+                this.criaErro(campo, 'Campo tal não pode estar em branco.');
+                valid = false;
+            }
+        }
+    }
+
+    criaErro(campo, msg){
+        const div = document.createElement('div');
+        div.innerHTML = msg;
+        div.classList.add('error-text');
+        campo.insertAdjacentElement('afterend', div);
+    }
+
 }
 
-const valida = ValidaFormulario();
+const valida = new ValidaFormulario();
